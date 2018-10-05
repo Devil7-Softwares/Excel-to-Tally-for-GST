2c38f801fb2457bb9eadd02b96523befd62581ea
Devil7DK <dineshthangavel47@gmail.com>
2018-10-05 13:17:16 +0530

    Setup MainForm, Add 'Party' Object

---
+++ GST Tally Helper/Classes/Objects/Party.vb
@@ -0,0 +1,46 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Namespace Objects
+    Public Class Party
+
+#Region "Properties/Fields"
+        Property Name As String
+        Property GSTIN As String
+        Property Type As Enums.PartyType
+#End Region
+
+#Region "Constructors"
+        Sub New()
+            Me.Name = ""
+            Me.GSTIN = ""
+            Me.Type = Enums.PartyType.SundryDebtor
+        End Sub
+
+        Sub New(ByVal Name As String, ByVal GSTIN As String, ByVal Type As Enums.PartyType)
+            Me.Name = Name
+            Me.GSTIN = GSTIN
+            Me.Type = Type
+        End Sub
+#End Region
+
+    End Class
+End Namespace
--- GST Tally Helper/frm_Main.Designer.vb
+++ GST Tally Helper/frm_Main.Designer.vb
@@ -20,57 +20,138 @@ Partial Class frm_Main
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.RibbonControl = New DevExpress.XtraBars.Ribbon.RibbonControl()
-        Me.RibbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
-        Me.RibbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.rp_PurchaseEntries = New DevExpress.XtraBars.Ribbon.RibbonPage()
+        Me.rpg_Items_Parties = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.rp_Parties = New DevExpress.XtraBars.Ribbon.RibbonPage()
+        Me.rpg_Items_Purchase = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
         Me.RibbonStatusBar = New DevExpress.XtraBars.Ribbon.RibbonStatusBar()
+        Me.gc_Parties = New DevExpress.XtraGrid.GridControl()
+        Me.gv_Parties = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.container_Tabs = New DevExpress.XtraTab.XtraTabControl()
+        Me.tp_PurchaseEntries = New DevExpress.XtraTab.XtraTabPage()
+        Me.tp_Parties = New DevExpress.XtraTab.XtraTabPage()
+        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gc_Parties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gv_Parties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.container_Tabs, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.container_Tabs.SuspendLayout()
+        Me.tp_Parties.SuspendLayout()
         Me.SuspendLayout()
-        ' 
-        ' RibbonControl
-        ' 
-        Me.RibbonControl.ApplicationIcon = Nothing
+        '
+        'RibbonControl
+        '
+        Me.RibbonControl.ExpandCollapseItem.Id = 0
+        Me.RibbonControl.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.RibbonControl.ExpandCollapseItem})
         Me.RibbonControl.Location = New System.Drawing.Point(0, 0)
+        Me.RibbonControl.MaxItemId = 1
         Me.RibbonControl.Name = "RibbonControl"
-        Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.RibbonPage1})
-        Me.RibbonControl.SelectedPage = Me.RibbonPage1
-        Me.RibbonControl.Size = New System.Drawing.Size(442, 141)
-        ' 
-        ' RibbonPage1
-        ' 
-        Me.RibbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.RibbonPageGroup1})
-        Me.RibbonPage1.Name = "RibbonPage1"
-        Me.RibbonPage1.Text = "RibbonPage1"
-        ' 
-        ' RibbonPageGroup1
-        ' 
-        Me.RibbonPageGroup1.Name = "RibbonPageGroup1"
-        Me.RibbonPageGroup1.Text = "RibbonPageGroup1"
-        ' 
-        ' RibbonStatusBar
-        ' 
-        Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 425)
+        Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.rp_PurchaseEntries, Me.rp_Parties})
+        Me.RibbonControl.Size = New System.Drawing.Size(442, 143)
+        Me.RibbonControl.StatusBar = Me.RibbonStatusBar
+        '
+        'rp_PurchaseEntries
+        '
+        Me.rp_PurchaseEntries.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.rpg_Items_Purchase})
+        Me.rp_PurchaseEntries.Name = "rp_PurchaseEntries"
+        Me.rp_PurchaseEntries.Text = "Purchase Entries"
+        '
+        'rpg_Items_Parties
+        '
+        Me.rpg_Items_Parties.Name = "rpg_Items_Parties"
+        Me.rpg_Items_Parties.ShowCaptionButton = False
+        Me.rpg_Items_Parties.Text = "Items"
+        '
+        'rp_Parties
+        '
+        Me.rp_Parties.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.rpg_Items_Parties})
+        Me.rp_Parties.Name = "rp_Parties"
+        Me.rp_Parties.Text = "Parties"
+        '
+        'rpg_Items_Purchase
+        '
+        Me.rpg_Items_Purchase.Name = "rpg_Items_Purchase"
+        Me.rpg_Items_Purchase.ShowCaptionButton = False
+        Me.rpg_Items_Purchase.Text = "Items"
+        '
+        'RibbonStatusBar
+        '
+        Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 418)
         Me.RibbonStatusBar.Name = "RibbonStatusBar"
         Me.RibbonStatusBar.Ribbon = Me.RibbonControl
-        Me.RibbonStatusBar.Size = New System.Drawing.Size(442, 24)
-        ' 
-        ' RibbonForm1
-        ' 
+        Me.RibbonStatusBar.Size = New System.Drawing.Size(442, 31)
+        '
+        'gc_Parties
+        '
+        Me.gc_Parties.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gc_Parties.Location = New System.Drawing.Point(0, 0)
+        Me.gc_Parties.MainView = Me.gv_Parties
+        Me.gc_Parties.MenuManager = Me.RibbonControl
+        Me.gc_Parties.Name = "gc_Parties"
+        Me.gc_Parties.Size = New System.Drawing.Size(436, 247)
+        Me.gc_Parties.TabIndex = 2
+        Me.gc_Parties.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gv_Parties})
+        '
+        'gv_Parties
+        '
+        Me.gv_Parties.GridControl = Me.gc_Parties
+        Me.gv_Parties.Name = "gv_Parties"
+        '
+        'container_Tabs
+        '
+        Me.container_Tabs.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.container_Tabs.HeaderLocation = DevExpress.XtraTab.TabHeaderLocation.Bottom
+        Me.container_Tabs.Location = New System.Drawing.Point(0, 143)
+        Me.container_Tabs.Name = "container_Tabs"
+        Me.container_Tabs.SelectedTabPage = Me.tp_PurchaseEntries
+        Me.container_Tabs.Size = New System.Drawing.Size(442, 275)
+        Me.container_Tabs.TabIndex = 3
+        Me.container_Tabs.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.tp_PurchaseEntries, Me.tp_Parties})
+        '
+        'tp_PurchaseEntries
+        '
+        Me.tp_PurchaseEntries.Name = "tp_PurchaseEntries"
+        Me.tp_PurchaseEntries.Size = New System.Drawing.Size(436, 247)
+        Me.tp_PurchaseEntries.Text = "Purchase Entries"
+        '
+        'tp_Parties
+        '
+        Me.tp_Parties.Controls.Add(Me.gc_Parties)
+        Me.tp_Parties.Name = "tp_Parties"
+        Me.tp_Parties.Size = New System.Drawing.Size(436, 247)
+        Me.tp_Parties.Text = "Parties"
+        '
+        'frm_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(442, 449)
+        Me.Controls.Add(Me.container_Tabs)
         Me.Controls.Add(Me.RibbonStatusBar)
         Me.Controls.Add(Me.RibbonControl)
-
+        Me.Name = "frm_Main"
         Me.Ribbon = Me.RibbonControl
         Me.StatusBar = Me.RibbonStatusBar
         Me.Text = "RibbonForm1"
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gc_Parties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gv_Parties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.container_Tabs, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.container_Tabs.ResumeLayout(False)
+        Me.tp_Parties.ResumeLayout(False)
         Me.ResumeLayout(False)
-
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents RibbonControl As DevExpress.XtraBars.Ribbon.RibbonControl
-    Friend WithEvents RibbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
-    Friend WithEvents RibbonPageGroup1 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents rp_PurchaseEntries As DevExpress.XtraBars.Ribbon.RibbonPage
+    Friend WithEvents rpg_Items_Parties As DevExpress.XtraBars.Ribbon.RibbonPageGroup
     Friend WithEvents RibbonStatusBar As DevExpress.XtraBars.Ribbon.RibbonStatusBar
-
-
+    Friend WithEvents rp_Parties As DevExpress.XtraBars.Ribbon.RibbonPage
+    Friend WithEvents rpg_Items_Purchase As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents gc_Parties As DevExpress.XtraGrid.GridControl
+    Friend WithEvents gv_Parties As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents container_Tabs As DevExpress.XtraTab.XtraTabControl
+    Friend WithEvents tp_PurchaseEntries As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents tp_Parties As DevExpress.XtraTab.XtraTabPage
 End Class

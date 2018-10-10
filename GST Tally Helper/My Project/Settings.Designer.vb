5760e725aada1e68473253c3508603fc869fc519
Devil7DK <dineshthangavel47@gmail.com>
2018-10-10 16:42:17 +0530

    Add XtraFormTemp Class

--- GST Tally Helper/My Project/Settings.Designer.vb
+++ GST Tally Helper/My Project/Settings.Designer.vb
@@ -89,6 +89,18 @@ Namespace My
                 Me("CalculateValues") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property Skin() As String
+            Get
+                Return CType(Me("Skin"),String)
+            End Get
+            Set
+                Me("Skin") = value
+            End Set
+        End Property
     End Class
 End Namespace
 

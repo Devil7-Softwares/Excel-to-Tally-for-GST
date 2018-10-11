3d34180880ba9c0ec267b5b5033973b01325f5c0
Devil7DK <dineshthangavel47@gmail.com>
2018-10-11 22:45:14 +0530

    Add Splash Screen

--- E2TGST/My Project/Application.Designer.vb
+++ E2TGST/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.D7.Automation.E2TGST.frm_Main
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.D7.Automation.E2TGST.frm_Splash
+        End Sub
     End Class
 End Namespace

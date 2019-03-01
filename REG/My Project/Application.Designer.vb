0a72e1f18c49bc794ff80bcc083f0adf5e1f4a8d
Devil7DK <dineshthangavel47@gmail.com>
2019-03-01 15:56:33 +0530

    REG: Set Startup Form

--- REG/My Project/Application.Designer.vb
+++ REG/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.D7.Automation.E2TGST.REG.Form1
+            Me.MainForm = Global.D7.Automation.E2TGST.REG.frm_Main
         End Sub
     End Class
 End Namespace
